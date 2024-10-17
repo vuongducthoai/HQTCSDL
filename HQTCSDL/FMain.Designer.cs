31e1fc2ae268eaa61d2c5046eef43035973c94c2
= <=>
2024-10-17 14:28:51 +0700

    123

--- HQTCSDL/FMain.Designer.cs
+++ HQTCSDL/FMain.Designer.cs
@@ -31,9 +31,8 @@ namespace HQTCSDL
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button6 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,39 +41,36 @@ namespace HQTCSDL
             this.button5 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.headerPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel2
+            // mainPanel
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.panel2.Location = new System.Drawing.Point(243, 54);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(920, 421);
-            this.panel2.TabIndex = 1;
+            this.mainPanel.BackColor = System.Drawing.Color.Transparent;
+            this.mainPanel.Controls.Add(this.headerPanel);
+            this.mainPanel.Controls.Add(this.controlPanel);
+            this.mainPanel.Location = new System.Drawing.Point(243, 11);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(927, 651);
+            this.mainPanel.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 2);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(234, 40);
+            this.button1.Size = new System.Drawing.Size(234, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "Sản Phẩm";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::HQTCSDL.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(-2, 6);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(239, 97);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
             // 
             // flowLayoutPanel1
             // 
@@ -86,103 +82,146 @@ namespace HQTCSDL
             this.flowLayoutPanel1.Controls.Add(this.button5);
             this.flowLayoutPanel1.Controls.Add(this.button7);
             this.flowLayoutPanel1.Controls.Add(this.button8);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(-2, 105);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-2, 136);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(239, 370);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(239, 526);
             this.flowLayoutPanel1.TabIndex = 4;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(3, 46);
+            this.button6.Location = new System.Drawing.Point(3, 66);
             this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(234, 40);
+            this.button6.Size = new System.Drawing.Size(234, 60);
             this.button6.TabIndex = 5;
             this.button6.Text = "Danh mục";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 90);
+            this.button2.Location = new System.Drawing.Point(3, 130);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(234, 40);
+            this.button2.Size = new System.Drawing.Size(234, 60);
             this.button2.TabIndex = 1;
             this.button2.Text = "Khách hàng";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 134);
+            this.button3.Location = new System.Drawing.Point(3, 194);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(234, 40);
+            this.button3.Size = new System.Drawing.Size(234, 60);
             this.button3.TabIndex = 2;
             this.button3.Text = "Nhà cung cấp";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(3, 178);
+            this.button4.Location = new System.Drawing.Point(3, 258);
             this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(234, 40);
+            this.button4.Size = new System.Drawing.Size(234, 60);
             this.button4.TabIndex = 3;
             this.button4.Text = "Đơn hàng";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(3, 222);
+            this.button5.Location = new System.Drawing.Point(3, 322);
             this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(234, 40);
+            this.button5.Size = new System.Drawing.Size(234, 60);
             this.button5.TabIndex = 4;
             this.button5.Text = "Nhập hàng";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(3, 266);
+            this.button7.Location = new System.Drawing.Point(3, 386);
             this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(234, 40);
+            this.button7.Size = new System.Drawing.Size(234, 60);
             this.button7.TabIndex = 6;
             this.button7.Text = "Ưu đãi";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(3, 310);
+            this.button8.Location = new System.Drawing.Point(3, 450);
             this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(234, 40);
+            this.button8.Size = new System.Drawing.Size(234, 60);
             this.button8.TabIndex = 7;
             this.button8.Text = "Thống kê";
             this.button8.UseVisualStyleBackColor = true;
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.BackColor = System.Drawing.Color.LightCyan;
+            this.controlPanel.Location = new System.Drawing.Point(3, 74);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(924, 574);
+            this.controlPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HQTCSDL.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.GhostWhite;
+            this.headerPanel.Controls.Add(this.label1);
+            this.headerPanel.Location = new System.Drawing.Point(3, 3);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(921, 65);
+            this.headerPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(400, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome";
             // 
             // FMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1163, 470);
+            this.ClientSize = new System.Drawing.Size(1182, 673);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FMain";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.mainPanel.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Panel panel2;
+        private Panel mainPanel;
         private Button button1;
         private PictureBox pictureBox1;
         private FlowLayoutPanel flowLayoutPanel1;
@@ -193,5 +232,8 @@ namespace HQTCSDL
         private Button button6;
         private Button button7;
         private Button button8;
+        private Panel controlPanel;
+        private Panel headerPanel;
+        private Label label1;
     }
 }
