2ad1a6733d9168625b705ca2631a0cdebd8da989
= <=>
2024-10-21 16:47:57 +0700

    123

--- HQTCSDL/Program.cs
+++ HQTCSDL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HQTCSDL.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace HQTCSDL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FMain());
+            Application.Run(new FSupplier());
         }
     }
 }
