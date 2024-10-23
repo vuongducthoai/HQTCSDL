ee74992f3600b87938d4e7a447dc9ec7fcf47e61
= <=>
2024-10-23 16:58:12 +0700

    123

--- HQTCSDL/Program.cs
+++ HQTCSDL/Program.cs
@@ -17,7 +17,8 @@ namespace HQTCSDL
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FOrder());
-            Application.Run(new Forms.Login.FLogin());
+            //Application.Run(new Forms.Login.FLogin());
+            Application.Run(new Forms.ManagementStatistic.StatisticChart());
 
         }
     }
